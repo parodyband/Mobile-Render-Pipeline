256364aacb92104609edbf9df71569235859847d
Austin Crane <parodyband@gmail.com>
2023-11-15 00:54:49 -0600

    update

--- Mobile SRP/ShaderLibrary/DitheringFunctions.hlsl
+++ Mobile SRP/ShaderLibrary/DitheringFunctions.hlsl
@@ -16,23 +16,20 @@ real BlueNoiseSampler(real2 uv)
     return SAMPLE_TEXTURE2D(_BlueNoiseTextureLLL512, sampler_BlueNoiseTextureLLL512, uv).r;
 }
 
-real4x4 bayerMatrix = real4x4(
-    0,  8,  2, 10,
-    12,  4, 14,  6,
-    3, 11,  1,  9,
-    15,  7, 13,  5
-);
+real BayerDither4x4(real2 position, real brightness) {
+    int4x4 bayerMatrix = int4x4( 0,  8,  2, 10,
+                                12,  4, 14,  6,
+                                 3, 11,  1,  9,
+                                15,  7, 13,  5);
 
-real BayerDither(real2 uv, real ditherAmount)
-{
-    // Scale UV to the size of the Bayer matrix
-    int2 bayerPos = int2(frac(uv) * 4);
+    int2 pos = int2(fmod(position.x, 4), fmod(position.y, 4));
 
-    // Sample the Bayer matrix value
-    real bayerValue = bayerMatrix[bayerPos.x][bayerPos.y] / 15.0; // Normalizing
-
-    // Apply dithering
-    return bayerValue * ditherAmount;
+    const real ditheredBrightness = brightness * 16.0;
+    if (ditheredBrightness > bayerMatrix[pos.y][pos.x]) {
+        return 1.0;
+    } else {
+        return 0.0;
+    }
 }
 
 
