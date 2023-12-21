1b06bb85544d5187e59f73a504b19696f0a0d94c
Austin Crane <parodyband@gmail.com>
2023-12-20 20:59:38 -0600

    pbr enabled

--- Mobile SRP/Shaders/ShadowCasterPass.hlsl
+++ Mobile SRP/Shaders/ShadowCasterPass.hlsl
@@ -39,7 +39,7 @@ void ShadowCasterPassFragment (Varyings input) {
     InputConfig config = GetInputConfig(input.baseUV);
     float4 base = GetBase(input.baseUV);
     #if defined(_SHADOWS_CLIP)
-    clip(base.a - GetCutoff(config));
+    clip(base.a - GetCutoff(input.baseUV));
     #elif defined(_SHADOWS_DITHER)
     float dither = InterleavedGradientNoise(input.positionCS.xy, 0);
     clip(base.a - dither);
