bf9a27ddf28b1253c18784dfb2783ca8ad28fe27
Austin Crane <parodyband@gmail.com>
2023-12-20 20:43:02 -0600

    pbr enabled

--- Mobile SRP/ShaderLibrary/BRDF.hlsl
+++ Mobile SRP/ShaderLibrary/BRDF.hlsl
@@ -12,12 +12,8 @@ struct BRDF {
 #define MIN_REFLECTIVITY 0.04
 
 float OneMinusReflectivity (float metallic) {
-    float range = 1.0 - MIN_REFLECTIVITY;
+    const float range = 1.0 - MIN_REFLECTIVITY;
     return range - metallic * range;
-}
-
-float srgbToLinear (float srgb) {
-    return clamp(srgb <= 0.04045 ? srgb / 12.92 : pow((srgb + 0.055) / 1.055, 2.4),.05,.95);
 }
 
 BRDF GetBRDF (Surface surface, bool applyAlphaToDiffuse = false) {
@@ -30,21 +26,20 @@ BRDF GetBRDF (Surface surface, bool applyAlphaToDiffuse = false) {
     }
     brdf.specular = lerp(MIN_REFLECTIVITY, surface.color, surface.metallic);
 
-    brdf.perceptualRoughness =
-        PerceptualSmoothnessToPerceptualRoughness(srgbToLinear(surface.smoothness));
-    brdf.roughness = PerceptualRoughnessToRoughness(brdf.perceptualRoughness);
+    brdf.perceptualRoughness = PerceptualSmoothnessToPerceptualRoughness(surface.smoothness);
+    brdf.roughness = clamp(PerceptualRoughnessToRoughness(brdf.perceptualRoughness) * brdf.perceptualRoughness, .001, .99);
 	
     brdf.fresnel = saturate(surface.smoothness + 1.0 - oneMinusReflectivity);
     return brdf;
 }
 
 float SpecularStrength (Surface surface, BRDF brdf, Light light) {
-    float3 h = SafeNormalize(light.direction + surface.viewDirection);
-    float nh2 = Square(saturate(dot(surface.normal, h)));
-    float lh2 = Square(saturate(dot(light.direction, h)));
-    float r2 = Square(brdf.roughness);
-    float d2 = Square(nh2 * (r2 - 1.0) + 1.00001);
-    float normalization = brdf.roughness * 4.0 + 2.0;
+    const float3 h = SafeNormalize(light.direction + surface.viewDirection);
+    const float nh2 = Square(saturate(dot(surface.normal, h)));
+    const float lh2 = Square(saturate(dot(light.direction, h)));
+    const float r2 = Square(brdf.roughness);
+    const float d2 = Square(nh2 * (r2 - 1.0) + 1.00001);
+    const float normalization = brdf.roughness * 4.0 + 2.0;
     return r2 / (d2 * max(0.1, lh2) * normalization);
 }
 
