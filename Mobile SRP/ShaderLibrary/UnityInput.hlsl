da4005e0247473d8bb41377b880c512f9b480ccf
Austin Crane <parodyband@gmail.com>
2024-03-04 21:21:15 -0600

    Update UnityInput.hlsl

--- Mobile SRP/ShaderLibrary/UnityInput.hlsl
+++ Mobile SRP/ShaderLibrary/UnityInput.hlsl
@@ -27,12 +27,13 @@ CBUFFER_START(UnityPerDraw)
 	float4 unity_SHBg;
 	float4 unity_SHBb;
 	float4 unity_SHC;
-#endif
 
 	float4 unity_ProbeVolumeParams;
 	float4x4 unity_ProbeVolumeWorldToObject;
 	float4 unity_ProbeVolumeSizeInv;
 	float4 unity_ProbeVolumeMin;
+#endif
+
 CBUFFER_END
 
 float4x4 unity_MatrixVP;
