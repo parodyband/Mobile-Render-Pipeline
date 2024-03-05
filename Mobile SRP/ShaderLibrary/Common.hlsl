6eb6022ff53281d887472a9014c9d7cd8763a9f2
Austin Crane <parodyband@gmail.com>
2024-03-05 00:28:23 -0600

    Update Common.hlsl

--- Mobile SRP/ShaderLibrary/Common.hlsl
+++ Mobile SRP/ShaderLibrary/Common.hlsl
@@ -94,4 +94,18 @@ float2 ScreenSpaceUV(float2 PositionCSXY) {
     
 	return screenUV;
 }
+
+inline float4 UnityObjectToClipPos(in float3 pos)
+{
+	#if defined(STEREO_CUBEMAP_RENDER_ON)
+	return UnityObjectToClipPosODS(pos);
+	#else
+	// More efficient than computing M*VP matrix product
+	return mul(UNITY_MATRIX_VP, mul(unity_ObjectToWorld, float4(pos, 1.0)));
+	#endif
+}
+inline float4 UnityObjectToClipPos(float4 pos) // overload for float4; avoids "implicit truncation" warning for existing shaders
+{
+	return UnityObjectToClipPos(pos.xyz);
+}
 #endif
